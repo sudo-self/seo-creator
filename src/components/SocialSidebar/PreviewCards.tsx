adce3389e9b4cf857c6b1afe736cec18e369b2a3
Sudo-Self <sudo-self@proton.me>
2024-05-25 20:31:07 -0600

    Update PreviewCards.tsx

--- src/components/SocialSidebar/PreviewCards.tsx
+++ src/components/SocialSidebar/PreviewCards.tsx
@@ -8,19 +8,13 @@ import { useSocialPreviewStates } from 'hooks/useSocialPreviewStates';
 export default function PreviewCards() {
   const {
     twitterPreview,
-    linkedinPreview,
-    pinterestPreview,
-    discordPreview,
     facebookPreview,
     googlePreview,
-    slackPreview,
+  
     setTwitterPreview,
-    setLinkedinPreview,
-    setPinterestPreview,
-    setDiscordPreview,
     setFacebookPreview,
     setGooglePreview,
-    setSlackPreview,
+ 
   } = useSocialPreviewStates();
 
   return (
@@ -28,22 +22,10 @@ export default function PreviewCards() {
       <SocialBtn setState={setGooglePreview} state={googlePreview} icon={<BsGoogle />} text="Google" />
       <SocialBtn setState={setFacebookPreview} state={facebookPreview} icon={<FaFacebookF />} text="Facebook" />
       <SocialBtn setState={setTwitterPreview} state={twitterPreview} icon={<GrTwitter />} text="Twitter" />
-      <SocialBtn setState={setLinkedinPreview} state={linkedinPreview} icon={<FaLinkedinIn />} text="Linkedin" />
-      <SocialBtn
-        setState={setPinterestPreview}
-        state={pinterestPreview}
-        icon={<FaPinterestP />}
-        text="Pinterest"
-        showWarnMsg={true}
-      />
-      <SocialBtn
-        setState={setDiscordPreview}
-        state={discordPreview}
-        icon={<FaDiscord />}
-        text="Discord"
-        showWarnMsg={true}
-      />
-      <SocialBtn setState={setSlackPreview} state={slackPreview} icon={<BsSlack />} text="Slack" showWarnMsg={true} />
+  
+     
+     
+     
     </VStack>
   );
 }
