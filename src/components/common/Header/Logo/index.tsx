c3cbd386b770e05bc9404a3796171faed8e157da
Sudo-Self <sudo-self@proton.me>
2024-05-25 22:01:45 -0600

    Update index.tsx

--- src/components/common/Header/Logo/index.tsx
+++ src/components/common/Header/Logo/index.tsx
@@ -1,20 +1,18 @@
 import NextLink from 'next/link';
-import { Flex, Heading, Text, Link } from '@chakra-ui/react';
-import { SiSpringCreators } from 'react-icons/si';
+import { Flex, Heading, Text, Link, Image } from '@chakra-ui/react';
 
 export default function Logo() {
   return (
     <NextLink href="/" passHref>
       <Link display="flex" alignItems="center" gridGap="0.3rem" _hover={{ textDecoration: 'none' }}>
-        <Text
-          color="fill.300"
-          p="0.3rem"
+        <Image
+          src="https://api.iconify.design/simple-icons:jrgroup.svg?color=%235e30eb" 
+          alt="SEO Creator Logo"
+          boxSize="2rem"
+          objectFit="contain"
           transition="0.25s ease-out"
-          borderRadius="10px"
-          _hover={{ background: 'fill.300', color: 'white' }}
-        >
-          <SiSpringCreators size="2rem" />
-        </Text>
+          _hover={{ transform: 'scale(1.1)' }} 
+        />
         <Heading as="h1" size="md" userSelect="none">
           SEO CREATOR
         </Heading>
@@ -22,4 +20,5 @@ export default function Logo() {
     </NextLink>
   );
 }
+
 
