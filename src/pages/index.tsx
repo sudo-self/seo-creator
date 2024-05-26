d1fc58945f0a17daa5895b853b37944fa1fe592c
Sudo-Self <sudo-self@proton.me>
2024-05-25 22:17:22 -0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 import SEO from 'components/common/SEO';
-import { Box } from '@chakra-ui/react';
 import LeftSidebar from 'components/LeftSidebar';
 import RightSidebar from 'components/RightSidebar';
 import SocialSidebar from 'components/SocialSidebar';
 import { useShowPreviewCtx } from 'context/ShowPreview';
 import MinimalSidebar from 'components/RightSidebar/MinimalSidebar';
 
-export default function Main() {
+const Main: React.FC = () => {
   const { showPreview } = useShowPreviewCtx();
+
+  useEffect(() => {
+  
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = '/favicon.ico'; 
+    document.head.appendChild(link);
+  }, []);
 
   return (
     <Box
@@ -24,4 +34,13 @@ export default function Main() {
       {showPreview ? <RightSidebar /> : <MinimalSidebar />}
     </Box>
   );
-}
+};
+
+const App: React.FC = () => (
+  <ChakraProvider>
+    <Main />
+  </ChakraProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
